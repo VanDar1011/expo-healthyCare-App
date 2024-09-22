3cc1f59f56994b11ee4ac53a2d9a6fe9348f45ee
dat <datthao27042002@gmail.com>
2024-09-22 09:40:56 +0700

    use .env

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 import Constants from "expo-constants";
-const API_APP_URL = Constants.expoConfig.extra.apiUrl;
-console.log(API_APP_URL);
-export default API_APP = API_APP_URL;
+const API_APP = Constants.expoConfig.extra.apiUrl;
+
+console.log(API_APP);
+export default API_APP;
